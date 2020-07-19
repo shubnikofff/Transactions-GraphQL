a96f3e80051aadf2b6d897602a27ea96e9811ef4
Alexey Shubnikov <shubnikov.av@gmail.com>
2020-07-19 20:25:53 +0300

    Made applications deployable

--- admin-panel/src/apolloClient.ts
+++ admin-panel/src/apolloClient.ts
@@ -2,7 +2,7 @@ import { ApolloClient } from 'apollo-client';
 import { InMemoryCache, NormalizedCacheObject } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
 
-const URI = 'http://localhost:4000/';
+const URI = `http://${process.env.SERVER_URI || 'localhost:4000'}/`;
 
 const cache = new InMemoryCache();
 const link = new HttpLink({
